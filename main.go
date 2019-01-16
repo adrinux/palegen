f3e00e75d2683c6eaf15472f4b6c24162760b6fd
Adrian Simmons <adrian@perlucida.com>
2019-01-16 17:08:57 +0000

    Add a map and WIP to assign hues to map

--- main.go
+++ main.go
@@ -21,6 +21,27 @@ import (
 	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
+var (
+	h    float64
+	c    float64
+	l    float64
+	hues = map[string]float64{
+		"red":     0, // and 360
+		"orange":  30,
+		"yellow":  60,
+		"lime":    90,
+		"green":   120,
+		"teal":    150,
+		"cyan":    180,
+		"blue":    210,
+		"indigo":  240,
+		"violet":  270,
+		"fuschia": 300,
+		"pink":    330,
+		"grey":    360, // desaturate input to create a grey
+	}
+)
+
 func main() {
 	// ic = input color as CSS style hexadecimal triplet
 	ic := "#" + os.Args[1]
@@ -28,20 +49,39 @@ func main() {
 	// hc = convert input to colorful.color
 	hc, _ := colorful.Hex(ic)
 
-	// colorful.color converted to hcl space (hue, chroma, lightness)
+	// colorful.Color converted to hcl space (hue, chroma, lightness)
 	h, c, l := hc.Hcl()
 
 	fmt.Println("Input color:", ic)
 	fmt.Println("Converted to HCL space:", h, c, l)
 
+	genHues(h, c, l)
+
+}
+
+func genHues(h, c, l float64) {
 	// generate hues
 	// rotate the hue value around the full 360 degress with 12 steps
 	steps := 12
 	size := float64(360 / steps)
 
 	for i := 0; i < steps; i++ {
-		h = h + size
+		if (h + size) <= 360 {
+			h = h + size
+		} else if (h + size) > 360 {
+			h = (h + size) - 360
+		}
 		fmt.Printf("hcl is: %v %v %v\n", h, c, l)
+
+		switch {
+		case h >= 0 && h <= 30:
+			// add to the hues map for the red key
+			fmt.Println("Assigned to: red")
+		case h > 30 && h <= 60:
+			// add to the hues map for the red key
+			fmt.Println("Assigned to: orange")
+		}
+
 	}
 
 	// need these in a map
